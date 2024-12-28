4daee4e7c8289b5a4573decfc5977c68decb9385
Syed Rizwan Hussain <106629183+Newcoderdas@users.noreply.github.com>
2024-12-29 02:18:28 +0500

    loader

--- src/app/(pages)/home/page.tsx
+++ src/app/(pages)/home/page.tsx
@@ -1,26 +1,41 @@
+"use client"
+import React, { useState, useEffect } from 'react';
+import FAQ from '@/components/FAQ';
+import Features from '@/components/Features';
+import HeroBanner from '@/components/HeroBanner';
+import Chamber from '@/components/Chamber';
+import Image from 'next/image';
 
+const Page = () => {
+  const [isLoading, setIsLoading] = useState(true);
 
-// import Clients from '@/components/Clients'
+  useEffect(() => {
+    
+    const timer = setTimeout(() => {
+      setIsLoading(false);
+    }, 3000);
 
-import FAQ from '@/components/FAQ'
-import Features from '@/components/Features'
-import HeroBanner from '@/components/HeroBanner'
-import Chamber from '@/components/Chamber'
-import React from 'react'
+    return () => clearTimeout(timer); 
+  }, []);
 
+  return (
+    <>
+      {isLoading ? (
+        
+        <div className="flex items-center justify-center h-screen bg-gray-100">
+          <Image src="/warehouse.gif" height={100} width={100} alt="loader"/>
+        </div>
+      ) : (
+      
+        <>
+          <HeroBanner />
+          <Features />
+          <Chamber />
+          <FAQ />
+        </>
+      )}
+    </>
+  );
+};
 
-
-
-const page = () => {
-    return (
-      <>
-
-        <HeroBanner />
-        <Features/>
-         <Chamber/>
-       <FAQ/>
-      </>
-  )
-}
-
-export default page
+export default Page;
