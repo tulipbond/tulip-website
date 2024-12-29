126ae8401ad3469370226982e07058d18d795345
Syed Rizwan Hussain <106629183+Newcoderdas@users.noreply.github.com>
2024-12-29 16:35:56 +0500

    hello

--- src/app/(pages)/about/page.tsx
+++ src/app/(pages)/about/page.tsx
@@ -1,36 +1,35 @@
-"use client"
-import React from 'react'
-import HeroBanner from './components/HeroBanner'
-import Biography from './components/Biography'
-import Owners from "./components/Owners"
-import { useEffect, useState } from 'react'
-import Image from 'next/image'
+"use client";
 
+import React, { useEffect, useState } from 'react';
+import HeroBanner from './components/HeroBanner';
+import Biography from './components/Biography';
+import Owners from "./components/Owners";
+import Image from 'next/image';
 
-const page = () => {
-  const [isLoading, setIsLoading] = useState(true);
-    useEffect(() => {
-      
-      const timer = setTimeout(() => {
-        setIsLoading(false);
-      }, 3000);
-      return () => clearTimeout(timer); 
-    }, []);
-    return (
-      <>
-        {isLoading ? (
-          
-          <div className="flex items-center justify-center h-screen bg-gray-100">
-            <Image src="/warehouse.gif" height={100} width={100} alt="loader"/>
-          </div>
-        ) : (
-      <>
-      <HeroBanner />
-        <Biography />
-        <Owners/>
-      </>
-  )}
-  </>
-    );
-  };
-export default page
+const Page: React.FC = () => {
+  const [isLoading, setIsLoading] = useState<boolean>(true);
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setIsLoading(false);
+    }, 3000);
+    return () => clearTimeout(timer); // Clean up the timeout
+  }, []);
+
+  return (
+    <>
+      {isLoading ? (
+        <div className="flex items-center justify-center h-screen bg-gray-100">
+          <Image src="/warehouse.gif" height={100} width={100} alt="loader" />
+        </div>
+      ) : (
+        <>
+          <HeroBanner />
+          <Biography />
+          <Owners />
+        </>
+      )}
+    </>
+  );
+};
+
+export default Page;
